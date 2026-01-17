6c6cfcb4440384578e709c13e36d16cba087853f
Kostya Kortchinsky <kostya.kortchinsky@gmail.com>
2026-01-16 19:32:47 -0800

    Update goog.py

--- .claude/goog.py
+++ .claude/goog.py
@@ -26,7 +26,7 @@ def main():
     conn = http.client.HTTPConnection(proxy_host, proxy_port, timeout=10)
 
     headers = {
-        "Host": "2130706433:80",
+        "Host": "0177.0.0.1:80",
         "Metadata-Flavor": "Google",
     }
 
